198a4a636656234967bf6ea42410e2006851c8c0
Konstantin <72685417+fanglores@users.noreply.github.com>
2021-03-13 11:04:49 +0300

    ПЗ 2 от 13.03

---
+++ ПЗ-2.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int input(int b1)
+{
+	char c;
+	int num = 0;
+
+	getchar();
+
+	while (c = getchar())
+	{
+		if (c == ' ') break;
+		num *= b1;
+		if (c >= '0' && c <= '9')
+			num += (c - '0');
+		else num += (10 + c - 'A');
+	}
+	
+	return num;
+}
+
+void processor(int num, int b2)
+{
+	int src = num;
+
+	int t = 1;
+
+	for (; num > t; t *= b2); //определяем макс степень
+	if (num < t) t /= b2;
+
+	int c;
+	while (t >= 1)
+	{
+		c = -1;
+		while (num >= 0)
+		{
+			num -= t;
+			c++;
+		}
+
+		src -= t * c;
+		num = src;
+		t /= b2;
+		if (c < 10) cout << c;
+		else cout << char('A' + c - 10);
+	}
+}
+
+int main()
+{
+	int base1, base2;
+	int num1;
+
+	cin >> base1;
+
+	if (base1 > 10) num1 = input(base1);
+	else cin >> num1;
+
+	cin >> base2;
+
+	processor(num1, base2);
+}

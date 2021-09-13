e5754485f8af1190819f4e71285d27eda3d547a5
Konstantin <72685417+fanglores@users.noreply.github.com>
2021-09-13 12:02:29 +0300

    Update Task1.cpp
    3/4 options

--- 2021/Task1.cpp
+++ 2021/Task1.cpp
@@ -1,1 +1,100 @@
+#include <iostream>
+using namespace std;
 
+class ArrayClass
+{
+private:
+	int* p;
+	int size;
+
+public:
+	ArrayClass(int);
+	ArrayClass(const ArrayClass&);
+	void print();
+	~ArrayClass();
+	void set(int, int);
+	int get(int);
+	void push_back(int);
+};
+
+ArrayClass::ArrayClass(int size)
+{
+	this->size = size;
+	p = new int[size];
+}
+
+ArrayClass::ArrayClass(const ArrayClass& ac)
+{
+	size = ac.size;
+	p = new int[size];
+
+	for (int i = 0; i < size; i++)
+	{
+		p[i] = ac.p[i];
+	}
+}
+
+ArrayClass::~ArrayClass()
+{
+	delete[] p;
+}
+
+void ArrayClass::print()
+{
+	for (int i = 0; i < size; i++) cout << p[i] << " ";
+	cout << endl;
+}
+
+void ArrayClass::push_back(int val)
+{
+	int* new_mas = new int[size + 1];
+
+	for (int i = 0; i < size; i++) new_mas[i] = p[i];
+	new_mas[size] = val;
+
+	size++;
+	p = new_mas;
+}
+
+void ArrayClass::set(int pos, int val)
+{
+	if (pos < 0 || pos >= size)
+	{
+		cout << "Setter error. Array out of bounds" << endl;
+		return;
+	}
+	else if (val < -100 || val > 100)
+	{
+		cout << "Setter error. Value out of range" << endl;
+		return;
+	}
+	else p[pos] = val;
+}
+
+int ArrayClass::get(int pos)
+{
+	if (pos < 0 || pos >= size)
+	{
+		cout << "Getter error. Array out of bounds" << endl;
+		return -1;
+	}
+	else return p[pos];
+}
+
+int main()
+{
+	ArrayClass ac1(3);
+	for (int i = 0; i < 3; i++) ac1.set(i, i + 1);
+
+	ArrayClass ac2(ac1);
+
+	ac1.push_back(4);
+	ac1.print();
+	ac2.print();
+	cout << "Push back works\n" << endl;
+	ac1.set(1, 101); //Setter error
+	ac1.set(101, 1); //Setter error
+	ac1.get(101); //Getter error
+	cout << "Get 3rd element: " << ac1.get(2) << "\n" << endl;
+
+}

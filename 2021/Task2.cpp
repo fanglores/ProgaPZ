c1a7ecb582302cd8e6a6edb93ac413c70024ebbf
Konstantin <72685417+fanglores@users.noreply.github.com>
2021-09-22 18:06:44 +0300

    Task2 init

---
+++ 2021/Task2.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include "strutil.h"
+#include "strutil.cpp"
+using namespace std;
+
+/*
+Написать функцию для определяет наличие строки a в строке str
+int strConteins(char *a, char* str)
+Возвращает -1, если не содержит
+Позицию начала, если содержит
+
+Примечание:
+1. Функция ищет только первое вхождение
+2. Программа должна включать следующие файлы:
+main.cpp – функции для тестирование strConteins
+strutil.cpp – реализация функции strConteins
+strutil.h - объявление функции strConteins
+*/
+
+
+int main()
+{
+	
+}

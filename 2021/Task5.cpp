19e3fee9e0e168beb8a9a09d1abbc833055b8c8a
Konstantin <72685417+fanglores@users.noreply.github.com>
2021-10-12 15:22:28 +0300

    Update Task5.cpp

--- 2021/Task5.cpp
+++ 2021/Task5.cpp
@@ -37,14 +37,13 @@ public:
 
 	void set(int, T)
 	{
-	
-	
+		//set algo
 		sort();
 	}
 
 	T get(int)
 	{
-	
+		//get algo
 		sort();
 	}
 
@@ -66,19 +65,36 @@ public:
 			cout << cur->data << endl;
 			cur = cur->next;
 		}
+	}
 
+	void unit_swap(Unit<T>& u1, Unit<T>& u2)
+	{
+		Unit<T>* pprev = u1->prev, pnext = u1->next;
+		T pdata = u1->data;
+
+		u1->prev = u2->prev;
+		u1->next = u2->next;
+		u1->data = u2->data;
+
+		u2->prev = pprev;
+		u2->next = pnext;
+		u2->data = pdata;
 	}
 
 	void sort()
-	{}
+	{
+		//sort algo
+	}
 };
 
 
 int main()
 {
 	List<int>* DataList = new List<int>();
+
 	DataList->push_back(1);
 	DataList->push_back(2);
 	DataList->push_back(3);
+
 	DataList->print();
 }

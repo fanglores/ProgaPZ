bb67f48818947e1df80fa7b10188ce5e8164f364
Konstantin <72685417+fanglores@users.noreply.github.com>
2021-10-16 08:42:42 +0300

    Update Task4.cpp
    Improved read algorithm

--- 2021/Task4.cpp
+++ 2021/Task4.cpp
@@ -3,41 +3,16 @@
 #include <iostream>
 using namespace std;
 
-void proc(string& str, map<string, int>& m)
-{
-	int ps, cs;
-	ps = cs = 0;
-
-	for (int i = 0; i < str.size(); i++)
-	{
-		if (str[i] == ' ')
-		{
-			
-			cs = i;
-
-			string sub = str.substr(ps, cs - ps);
-			if (m.find(sub) == m.end()) m.insert(make_pair(sub, 1));
-			else m[sub]++;
-
-			ps = cs + 1;
-		}
-	}
-
-	cs = str.size();
-	string sub = str.substr(ps, cs - ps);
-	if (m.find(sub) == m.end()) m.insert(make_pair(sub, 1));
-	else m[sub]++;
-
-}
-
 int main()
 {
 	map<string, int> dict;
-	string s;
+	string s = "ab";
 
-	getline(cin, s);
-
-	proc(s, dict);
+	while (cin >> s)
+	{
+		if (s == "/exit") break;
+		dict[s]++;
+	}
 
 	cout << endl;
 	for (pair<string, int> p : dict) cout << p.second << ' ' << p.first << endl;
